ca8ec2a623284fc85995ace9578d560fb991da1e
mcorthudayi <2203022083@student.beykent.edu.tr>
2026-05-04 20:43:56 +0300

    fix: EnableRetryOnFailure for Azure SQL transient errors

--- FinAware/Program.cs
+++ FinAware/Program.cs
@@ -13,7 +13,14 @@ builder.Services.AddSwaggerGen();
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(10),
+            errorNumbersToAdd: null
+        )
+    ));
 
 // CORS
 builder.Services.AddCors(options =>
